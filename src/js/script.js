41b61f181437f674aa83518b7d9ac6a48abe16d0
Hudson <hudson.b.araujo@gmail.com>
2026-03-18 23:59:39 -0300

    feat: calculadora de bhaskara adicionada

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+function Calcular(){
+    let a = document.getElementById("txtA").value;
+    let b = document.getElementById("txtB").value;
+    let c = document.getElementById("txtC").value;
+    if (a == "" || b == "" || c == "") {
+        alert("Insira todos os coeficientes.")
+    }
+    else {
+        let delta = Math.pow(b,2) - (4*a*c);
+
+        if (delta < 0) {
+            alert("A equação não possui raizes reais.");
+        }
+        else {
+            let raizA = (-b + Math.sqrt(delta)) / (2*a);
+            let raizB = (-b - Math.sqrt(delta)) / (2*a);
+            document.getElementById("txtDelta").value = delta.toFixed(2);
+            document.getElementById("txtRaizA").value = raizA.toFixed(2);
+            document.getElementById("txtRaizB").value = raizB.toFixed(2);
+        } 
+    }   
+}
